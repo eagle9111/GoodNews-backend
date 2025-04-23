62513250cf2f52300c3d72e15137b4b6623f9cc5
eagle9111 <alhassan.khalilnew@gmail.com>
2025-04-23 17:47:24 +0300

    fixing errors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // index.js
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import newsRoute from './routes/new-route.js';
 import cronJobs from './cron.js';
 import postsRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 
 const app = express();
